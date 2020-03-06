4528c21bca0a9315868ba7fd8a945d525650de57
Choey Cui <52060363+choeycui@users.noreply.github.com>
2020-03-06 15:33:47 +0800

    Update python_learning.py

--- python_learning.py
+++ python_learning.py
@@ -1,4 +1,28 @@
 '''
+historical
+'''
+
+#!!!
+'''
+Use 64bit pandas and numpy when load a very large data file, \
+otherwise there might raise the memory error because 32bit packages only support for maximum 2GB memory.
+'''
+
+'''
+SET the localhost 120.0.0.1, this can avoid the no-responding in calling Gaode API for http request.
+'''
+
+
+
+
+
+
+
+
+
+
+
+'''
 20200306
 
 DON'T initialize a dictionary with list/dict or other changable values when using fromkeys(), \
